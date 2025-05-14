a6c7162ad764a9bddefae17c57185f48cd3403aa
santiagoramirez11 <santiagoramirez11@gmail.com>
2025-05-13 22:30:11 -0400

    Update swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "swagger.yaml",
+    url: "https://raw.githubusercontent.com/santiagoramirez11/mic-stock-service/refs/heads/main/src/main/resources/openapi/openapi.yml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
